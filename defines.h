6c23d374f1f38f195988d10e8c891cf73bf2f7d4
Karol Herbst <git@karolherbst.de>
2015-02-27 22:25:58 +0100

    main: call tp_run_connection_manager with stub data

---
+++ defines.h
@@ -0,0 +1,4 @@
+#pragma once
+
+constexpr char TP_CM_ID[] = "steam";
+constexpr char TP_STEAM_VERSION[] = "0.0.0";
--- main.cpp
+++ main.cpp
@@ -1,4 +1,11 @@
+#include "defines.h"
+
+#include <telepathy-glib/run.h>
+
 int main(int argc, char *argv[])
 {
-
+	return tp_run_connection_manager(TP_CM_ID, TP_STEAM_VERSION, []() -> TpBaseConnectionManager *
+	{
+		return nullptr;
+	}, argc, argv);
 }
