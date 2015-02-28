a21a8d12cb73c6871663daceda16d88313e4969c
Karol Herbst <git@karolherbst.de>
2015-02-28 09:15:58 +0100

    connection-manager: added empty finalize method

--- connection-manager.cpp
+++ connection-manager.cpp
@@ -10,17 +10,11 @@ static void stema_connection_manager_constructed (GObject * object)
 	tp_base_connection_manager_add_protocol(TP_BASE_CONNECTION_MANAGER(object), steam_protocol_new());
 }
 
-static void steam_connection_manager_finalize (GObject * object)
-{
-	GLIB_CALL_PARENT(steam_connection_manager_parent_class, finalize, object);
-}
-
 void steam_connection_manager_class_init(SteamConnectionManagerClass * klass)
 {
 	GObjectClass *object_class = G_OBJECT_CLASS(klass);
 
 	object_class->constructed = stema_connection_manager_constructed;
-	object_class->finalize = steam_connection_manager_finalize;
 	klass->parent_class.cm_dbus_name = "steam";
 }
 
