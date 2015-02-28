8a7a24599e27256532faa60b184e30a9f6cb40fe
Karol Herbst <git@karolherbst.de>
2015-02-28 11:16:03 +0100

    protocol: stub implement required virtual methods

--- protocol.cpp
+++ protocol.cpp
@@ -1,11 +1,23 @@
 #include "glib-helpers.h"
 #include "protocol.h"
 
+static const TpCMParamSpec * get_parameters(TpBaseProtocol * self)
+{
+	return nullptr;
+}
+
+static TpBaseConnection * new_connection(TpBaseProtocol * self, GHashTable * asv, GError ** error)
+{
+	return nullptr;
+}
+
 G_DEFINE_TYPE(SteamProtocol, steam_protocol, TP_TYPE_BASE_PROTOCOL);
 
 void steam_protocol_class_init(SteamProtocolClass * klass)
 {
 	GObjectClass *object_class = G_OBJECT_CLASS(klass);
+	klass->parent_class.get_parameters = get_parameters;
+	klass->parent_class.new_connection = new_connection;
 }
 
 void steam_protocol_init(SteamProtocol * sp){}
