c7de4ee1a9da1ad7b80a67eaa06244daedc2d709
Karol Herbst <git@karolherbst.de>
2015-02-28 12:28:19 +0100

    connection: add stubs

---
+++ connection.cpp
@@ -0,0 +1,12 @@
+#include "connection.h"
+#include "glib-helpers.h"
+
+G_DEFINE_TYPE(SteamConnection, steam_connection, TP_TYPE_BASE_CONNECTION);
+
+void steam_connection_class_init(SteamConnectionClass * klass)
+{
+}
+
+void steam_connection_init(SteamConnection * scm)
+{
+}
---
+++ connection.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <glib-object.h>
+#include <telepathy-glib/base-connection.h>
+
+struct SteamConnection
+{
+	TpBaseConnection parent;
+};
+
+struct SteamConnectionClass
+{
+	TpBaseConnectionClass parent_class;
+};
+
+GType steam_connection_get_type();
+
+/* TYPE MACROS */
+#define STEAM_TYPE_CONNECTION \
+  (steam_connection_get_type ())
+#define STEAM_CONNECTION(obj) \
+  (G_TYPE_CHECK_INSTANCE_CAST((obj), STEAM_TYPE_CONNECTION, \
+                              SteamConnection))
+#define STEAM_CONNECTION_CLASS(klass) \
+  (G_TYPE_CHECK_CLASS_CAST((klass), STEAM_TYPE_CONNECTION, \
+                           SteamConnectionClass))
+#define STEAM_IS_CONNECTION(obj) \
+  (G_TYPE_CHECK_INSTANCE_TYPE((obj), STEAM_TYPE_CONNECTION))
+#define STEAM_IS_CONNECTION_CLASS(klass) \
+  (G_TYPE_CHECK_CLASS_TYPE((klass), STEAM_TYPE_CONNECTION))
+#define STEAM_CONNECTION_GET_CLASS(obj) \
+  (G_TYPE_INSTANCE_GET_CLASS ((obj), STEAM_TYPE_CONNECTION, \
+                              SteamConnectionClass))
