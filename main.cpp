7a738c7b83c37cf59cc45b628f849388ada556a4
Karol Herbst <git@karolherbst.de>
2015-02-28 07:06:58 +0100

    main: add STEAM_PERSIST env variable

--- main.cpp
+++ main.cpp
@@ -2,10 +2,18 @@
 
 #include "connection-manager.h"
 
+#include <cstdlib>
+
+#include <telepathy-glib/debug.h>
 #include <telepathy-glib/run.h>
 
 int main(int argc, char *argv[])
 {
+	if (std::getenv("STEAM_PERSIST") != nullptr)
+	{
+		tp_debug_set_persistent(true);
+	}
+
 	return tp_run_connection_manager(TP_CM_ID, TP_STEAM_VERSION, []()
 	{
 		return static_cast<TpBaseConnectionManager*>(g_object_new(steam_connection_manager_get_type(), NULL));
