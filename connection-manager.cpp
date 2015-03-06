9f709c3f6a1389b9c194622cb30311aa23bb6021
Karol Herbst <git@karolherbst.de>
2015-03-06 18:05:28 +0100

    connection-manager: fix typo

--- connection-manager.cpp
+++ connection-manager.cpp
@@ -4,7 +4,7 @@
 
 G_DEFINE_TYPE(SteamConnectionManager, steam_connection_manager, TP_TYPE_BASE_CONNECTION_MANAGER);
 
-static void stema_connection_manager_constructed (GObject * object)
+static void steam_connection_manager_constructed (GObject * object)
 {
 	GLIB_CALL_PARENT(G_OBJECT_CLASS(steam_connection_manager_parent_class)->constructed, object);
 
@@ -17,7 +17,7 @@ void steam_connection_manager_class_init(SteamConnectionManagerClass * klass)
 {
 	GObjectClass *object_class = G_OBJECT_CLASS(klass);
 
-	object_class->constructed = stema_connection_manager_constructed;
+	object_class->constructed = steam_connection_manager_constructed;
 	klass->parent_class.cm_dbus_name = "steam";
 }
 
